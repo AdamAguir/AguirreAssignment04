a5f0ff322a2ccda8b7ce38c47e72191dd3e6cd73
AdamAguir <71364286+AdamAguir@users.noreply.github.com>
2021-02-04 13:07:33 -0700

    MyCircle and My Triable getters and setters

--- MyCircle.java
+++ MyCircle.java
@@ -1,3 +1,32 @@
 public class MyCircle extends AbstractShape{
+    private int rad;
+
+    public MyCircle(int x, int y, int r){
+
+        super(x, y);
+        rad = r;
+    }
     
+    public String getCirCalc(){
+        \\TODO
+        return null;
+    }
+    //getters and setters
+    public int getRad(){
+        return rad;
+    }
+    public void setRad(int r){
+        rad = r;
+    }
+
+    @Override
+    public double getArea() {
+        
+        return (rad * rad * 3.14);
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
 }
--- MyTriangle.java
+++ MyTriangle.java
@@ -1,3 +1,42 @@
 public class MyTriangle extends AbstractShape{
+    
+    private double base;
+    private double height;
 
+    public MyTriangle(int x, int y, double b, double h){
+        super(x, y);
+        base = b;
+        height = h;
+    }
+
+    public String getTriCalc(){
+        //TODO
+        return null;
+    }
+
+    //getters and setters
+    public void setBase(double b){
+        base = b;
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getBase() {
+        return base;
+    }
+
+    @Override
+    public double getArea() {
+        return .5 * (base * height);
+    }
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    
 }
